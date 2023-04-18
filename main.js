9b26fdc068c5d01bd7436d120f3e38e95081c5d8
Eaarjun <91949706+Eaarjun@users.noreply.github.com>
2023-04-18 21:26:16 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+var select =document.querySelectorAll('.currency'),
+input_currency = document.getElementById('input_currency'),
+output_currency = document.getElementById('output_currency')
+//fetching the api
+const host = 'api.frankfurter.app';
+fetch(`https://${host}/currencies`)
+  .then(data => data.json())
+  .then((data) => {
+    const entries = Object.entries(data)
+    // console.log(entries);
+   for(i=0;i<entries.length;i++){
+    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`
+    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`
+   }
+    //alert(`10 GBP = ${data.rates.USD} USD`);
+  });
+
+  function convert(){
+    var input_currency_val = input_currency.value;
+    if(select[0].value != select[1].value){
+        // alert('Right')
+        const host = 'api.frankfurter.app';
+    fetch(`https://${host}/latest?amount=${input_currency_val}&from=${select[0].value}&to=${select[1].value}`)
+  .then(val => val.json())
+  .then((val) => {
+    output_currency.value = Object.values(val.rates)[0]
+  });
+    }else{
+        alert('Pls select 2 different currency')
+    }
+  }
